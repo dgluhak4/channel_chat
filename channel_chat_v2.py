0c690bab27e0f305759bb87010e5bccfd4483509
dgluhak4 <31708249+dgluhak4@users.noreply.github.com>
2019-08-08 23:25:49 +0200

    v2_hvar

--- channel_chat_v2.py
+++ channel_chat_v2.py
@@ -3,6 +3,7 @@ import sys
 import socket
 import threading
 import logging
+import random
 from time import sleep
 
 # GLOBALS
@@ -10,69 +11,47 @@ logging.basicConfig(level=logging.INFO,
                     format='[%(levelname)s] (%(threadName)-10s) %(message)s',
                    )
 
-BUFF_SIZE = 1024
+### channel/service network parameters
+BUFF_SIZE = 2048
 #HOST = 'chat.roundtriptime.com'
 HOST = sys.argv[1]
 LOCALHOST = '127.0.0.1'
 PORT = 11235
+
+client_ID = random.randrange(10001,99999)
+
+### message static values definitions 
 WELCOME = 'Welcome client '
 FAREWELL = 'Farewell client '
-EXIT_CODE = 'EXIT'
-NAME_CODE = 'NAME'
-CODE_LENGTH = 4
 CHANNEL_CODE = '\r\nCHANNEL INFO: '
 
-def client_channel(channel_client, client_list):
-    logging.info('Joined the channel')
-    exit_value = False
-# receving client message
-    while not exit_value:
-        client_msg = ''
-        try:
-            client_msg = channel_client.RecvMessage()
-            logging.debug('try '+str(channel_client.GetID())+client_msg)
-        except channel_client.GetError():
-            logging.debug('except '+str(channel_client.GetID())+client_msg)
-            channel_client.RemoveMessage(client_list)
-            sleep(1)
-        else:
-            logging.debug('else '+str(channel_client.GetID())+client_msg)
-            if len(client_msg) >= CODE_LENGTH:
-                if client_msg[:len(EXIT_CODE)] == EXIT_CODE:
-                    exit_value = True
-                    channel_client.ChannelMessage(CHANNEL_CODE+channel_client.GetName()+' leaves the channel\r\n')
-                elif client_msg[:len(NAME_CODE)] == NAME_CODE:
-                    channel_client.SetName(client_msg[(len(NAME_CODE)+1):(len(client_msg)-2)])
-                else:
-                    logging.debug('Adding: '+client_msg)
-                    channel_client.AddMessage(client_msg)
-            else:
-                logging.debug('Adding: '+client_msg)
-                channel_client.AddMessage(client_msg)
-# sending other clients messages
-        for peers in client_list:
-            if peers.GetID() != channel_client.GetID() and not peers.GetIfEmptyMessage():
-#        channel_client.SendMessage(peers.GetName()+': '+peers.GetMessage())
-                channel_client.SendMessage(peers.GetMessage())
-                peers.CountMessage()
-                client_list.remove(channel_client)
-                channel_client.SendMessage(FAREWELL+channel_client.GetName()+'\r\nHope to see you soon\r\n')
-                channel_client.Close()
-    return True
+### channel command definitions
+EXIT_CODE = 'EXIT'
+NAME_CODE = 'NAME'
+CODE_LENGTH = 5
 
+### Client object bitwise variable bit definitions
+NEW_OUTPUT_MESSAGE_INDICATOR = 0b00000001
+NEW_INPUT_MESSAGE_INDICATOR = 0b00000010
 
 class Client:
     def __init__(self, client_handle, client_IP, client_ID, name='Anon'):
-#    global in_message
 
+### Client object variables
         self.name = name
-        self.client_handle = client_handle
-        self.client_handle.setblocking(0)
-        self.in_message = ''
-        self.empty_message = True
-        self.count_message = 0
         self.IP = client_IP
         self.ID = client_ID
+        self.client_handle = client_handle
+        self.client_handle.setblocking(0)
+### Client object message related variables
+        self.in_message = ''
+        self.out_message = ''
+        self.in_message_status = False
+        self.out_message_status = False
+        self.empty_message = True
+        self.count_message = 0
+        self.receivers_list = []
+
 
     def GetIP(self):
         return self.IP
@@ -87,16 +66,47 @@ class Client:
         return self.ID
 
     def GetError(self):
-        return self.client_handle.error
+        return self.client_handle.OSerror
 #    return self.client_handle.error
 
     def GetIfEmptyMessage(self):
         return self.empty_message
 
+# proctiaj da li postoji nova poruka za poslati
+    def GetOutMsgStatus(self):
+        return self.out_message_status
+
+# procitaj da li postoji nova dolazna poruka
+    def GetInMsgStatus(self):
+        return self.in_message_status
+
+# procitaj listu primatelja poruke
+    def GetReceiversList(self):
+        return self.receivers_list
+
+# postavi da postoji nova poruka za poslati
+    def SetOutMsgStatus(self, status=True):
+        self.out_message_status = status
+
+# postavi da postoji nova dolazna poruka
+    def SetInMsgStatus (self, status=True):
+        self.in_message_status = status
+
+# postavi novo ime na kanalu
     def SetName(self, name_str):
         logging.debug('Name I '+name_str)
         self.ChannelMessage(CHANNEL_CODE+self.name+' changed into '+name_str+'\r\n')
         self.name = name_str
+
+# dodaj novi niz karaktera u buffer za odlaznu poruku
+    def OutputQueueMessage(self, msg_str):
+        self.out_message += msg_str
+        self.out_message_length = len(self.out_message)
+        return (len(self.out_message) <= BUFF_SIZE)
+
+# procitaj buffer za dolaynu poruku
+    def InputQueueMessage(self):
+        return self.in_message
 
     def ChannelMessage(self, channel_str):
         self.in_message += channel_str
@@ -120,10 +130,12 @@ class Client:
             self.empty_message = True
         return self.count_message
 
+# posalji poruku na klijenta
     def SendMessage(self, msg_str):
         self.client_handle.send(bytes(msg_str, 'utf-8'))
         logging.debug('send msg'+str(self.ID))
 
+# procitaj poruku od klijenta
     def RecvMessage(self):
         logging.debug('recv msg'+str(self.ID))
         return str(self.client_handle.recv(BUFF_SIZE), 'utf-8')
@@ -131,11 +143,57 @@ class Client:
     def CountMessage(self):
         self.count_message += 1
 
+# postavi socket u blocking ili non-blocking stanje
     def SetBlocking(self, state):
         self.client_handle.setblocking(state)
 
+# neagresivno iskljuci socket = kraj rada
+    def Shutdown(self):
+        self.client_handle.shutdown()
+
+# agresivno iskljuci socket
     def Close(self):
         self.client_handle.close()
+
+# glavni klijentski thread koji kontinuirana cita dolazne poruke i salje odlazne poruke
+    def ClientCoreLoop(self):
+        logging.info('Joined the channel')
+        exit_value = False
+# receving client message
+        while not exit_value:
+            client_msg = ''
+            try:
+                client_msg = self.RecvMessage()
+                logging.debug('try '+str(self.GetID())+client_msg)
+            #except channel_client.GetError():
+            except OSError:
+                logging.debug('except '+str(self.GetID())+client_msg)
+                self.RemoveMessage(client_list)
+                sleep(1)
+            else:
+                logging.debug('else '+str(self.GetID())+client_msg)
+                if len(client_msg) >= CODE_LENGTH:
+                    if client_msg[:len(EXIT_CODE)] == EXIT_CODE:
+                        exit_value = True
+                        self.ChannelMessage(CHANNEL_CODE+self.GetName()+' leaves the channel\r\n')
+                    elif client_msg[:len(NAME_CODE)] == NAME_CODE:
+                        self.SetName(client_msg[(len(NAME_CODE)+1):(len(client_msg)-2)])
+                    else:
+                        logging.debug('Adding: '+client_msg)
+                        self.AddMessage(client_msg)
+                else:
+                    logging.debug('Adding: '+client_msg)
+                    self.AddMessage(client_msg)
+# sending other clients messages
+            for peers in client_list:
+                if peers.GetID() != self.GetID() and not peers.GetIfEmptyMessage():
+#        channel_client.SendMessage(peers.GetName()+': '+peers.GetMessage())
+                    self.SendMessage(peers.GetMessage())
+                    peers.CountMessage()
+                    client_list.remove(self)
+                    self.SendMessage(FAREWELL+self.GetName()+'\r\nHope to see you soon\r\n')
+                    self.Close()
+        return True
 
 
 class Channel:
@@ -151,6 +209,7 @@ class Channel:
 # initialize chat-server
         self.channel_threads = []
         self.channel_sck = socket.socket()
+        self.client_list = {}        
         try:
             SERVER = (socket.gethostbyname(HOST), PORT)
         except OSError:
@@ -169,43 +228,61 @@ class Channel:
         Returns:
         - always True
         """
-        for channel in self.channel_threads:
+        for channel in self.client_list.values():
             channel.join(1)
             logging.shutdown()
             self.channel_sck.shutdown()
             self.channel_sck.close()
         return True
 
+    def NewClientAccept(self):
+        global client_ID
+        peers_message = ''
+        (client_handle, client_IP) = self.channel_sck.accept()
+        client_ID = client_ID+1
+        new_client = Client(client_handle, client_IP, client_ID, "User#"+str(client_ID))
+        #client_list.append(cl)
+# pokretanje novog threada
+        channel = threading.Thread(name='User#'+str(client_ID), target=new_client.ClientCoreLoop, args=())
+        #self.channel_threads.append(channel)
+        channel.start()
+        self.client_list[new_client] = channel
+# welcoming notes
+        new_client.OutputQueueMessage(WELCOME+'User#'+str(client_ID)+'\r\nType '+EXIT_CODE+' to end\r\nType '+NAME_CODE+' <chosen name> to identify yourself\r\n')
+        #client_handle.send(bytes(WELCOME+'User#'+str(client_ID)+'\r\nType '+EXIT_CODE+' to end\r\nType '+NAME_CODE+' <chosen name> to identify yourself\r\n', 'utf-8'))
+        clients_message = ''
+        for client in self.client_list:
+            clients_message += str(client.GetName())+' '
+        new_client.OutputQueueMessage('Currently on channel: '+clients_message+'\r\n')
+        new_client.out_message_status == True
+        #client_handle.send(bytes('Currently on channel: '+peers_message+'\r\n', 'utf-8'))
+
+    def DispatchChannelMessages(self):
+        for client in self.client_list:
+            if client.GetInMsgStatus():
+                for client_peer in client.GetReceiversList():
+                    client_peer.SetOutputQueueMessage(client.GetInputQueueMessage())
+                    client_peer.SetOutMsgStatus(True)
+                client.SetInMsgStatus(False)
+
     def CoreLoop(self):
         """Function that represents engine of the channel.
         Responsible for accepting new clients and maintaining the list of successfully joined clients.
         """
-        client_list = []
-        client_ID = 0
-        peers_message = ''
-# petlja za cekanje i aktiviranje klijenata
+# petlja za cekanje i aktiviranje klijenata i distribuciju poruka
         while True:
-            (client_handle, client_IP) = self.channel_sck.accept()
-            client_ID = client_ID+1
-            cl = Client(client_handle, client_IP, client_ID, "User#"+str(client_ID))
-            client_list.append(cl)
-# pokretanje novog threada
-            channel = threading.Thread(name='User#'+str(client_IP), target=client_channel, args=(cl, client_list,))
-            self.channel_threads.append(channel)
-            channel.start()
-# welcoming notes
-            client_handle.send(bytes(WELCOME+'User#'+str(client_ID)+'\r\nType '+EXIT_CODE+' to end\r\nType '+NAME_CODE+' <chosen name> to identify yourself\r\n', 'utf-8'))
-            peers_message = ''
-            for peers in client_list:
-                peers_message += str(peers.GetName())+' '
-                client_handle.send(bytes('Currently on channel: '+peers_message+'\r\n', 'utf-8'))
+            self.NewClientAccept()
+### message dispatcher between clients
+            self.DispatchChannelMessages()
         return True
 
-
-if __name__ == '__main__':
+def main():
     """Main function of the messaging program.
     Initializes a Channel class object, runs the engine of the channel and finally closes the channel.
     """
-    ch = Channel()
-    ch.CoreLoop()
-    ch.EndOfWork()
+    comm_channel = Channel()
+    comm_channel.CoreLoop()
+    comm_channel.EndOfWork()
+
+if __name__ == '__main__':
+    main()
